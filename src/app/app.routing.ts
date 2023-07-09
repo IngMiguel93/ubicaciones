e9582648e3dd1e30e01c6c7bfe8d5f961458debd
Viviana Ojeda <m.ojedan@uniandes.edu.co>
2023-07-09 11:18:40 -0500

    ultima actualización

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,40 +1,37 @@
-import { NgModule } from '@angular/core';
-import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { Routes, RouterModule } from "@angular/router";
 
-import { ComponentsComponent } from './components/components.component';
-import { ProfileComponent } from './examples/profile/profile.component';
-import { SignupComponent } from './examples/signup/signup.component';
-import { LandingComponent } from './examples/landing/landing.component';
-import { NoFoundComponent } from './components/no-found/no-found.component';
-import { TemploComponent } from './components/templo/templo.component';
-import { FormularioComponent } from './components/formulario/formulario.component';
-import { TrasmisionComponent } from './components/trasmision/trasmision.component';
+import { ComponentsComponent } from "./components/components.component";
+import { ProfileComponent } from "./examples/profile/profile.component";
+import { SignupComponent } from "./examples/signup/signup.component";
+import { LandingComponent } from "./examples/landing/landing.component";
+import { NoFoundComponent } from "./components/no-found/no-found.component";
+import { TemploComponent } from "./components/templo/templo.component";
+import { FormularioComponent } from "./components/formulario/formulario.component";
+import { TrasmisionComponent } from "./components/trasmision/trasmision.component";
+import { BrindisComponent } from "./components/brindis/brindis.component";
+import { AsadoComponent } from "./components/asado/asado.component";
 
-const routes: Routes =[
-    { path: '', redirectTo: '**', pathMatch: 'full' },
-    { path: 'home',             component: ComponentsComponent },
-    { path: 'user-profile',     component: ProfileComponent },
-    { path: 'signup',           component: SignupComponent },
-    { path: 'landing',          component: LandingComponent },
-    { path: 'templo',      component: TemploComponent },
-    { path: 'brindis',      component: NoFoundComponent },
-    { path: 'asado',      component: NoFoundComponent },
-    { path: 'trasmision',      component: TrasmisionComponent },
-    { path: 'formulario',      component: FormularioComponent },
-    { path: '**',      component: NoFoundComponent },
+const routes: Routes = [
+  { path: "", redirectTo: "**", pathMatch: "full" },
+  { path: "templo", component: TemploComponent },
+  { path: "brindis", component: BrindisComponent },
+  { path: "asado", component: AsadoComponent },
+  { path: "trasmision", component: TrasmisionComponent },
+  { path: "formulario", component: FormularioComponent },
+  { path: "**", component: NoFoundComponent },
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes,{
-      useHash: true
-    })
+    RouterModule.forRoot(routes, {
+      useHash: true,
+    }),
   ],
-  exports: [
-  ],
+  exports: [],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
